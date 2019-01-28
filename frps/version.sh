ee15f1d433aaa2a8bd665793ff2ee730ec1035a7
monokoo <monokoo@users.noreply.github.com>
2019-01-28 11:58:52 +0800

    lastest version support

--- frps/version.sh
+++ frps/version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export FRPS_VER=0.20.0
+latest_version=`curl -S  https://github.com/fatedier/frp/releases/latest | awk -F"/tag/v" '{print $2}' | awk -F"\">" '{print $1}'`
+export FRPS_VER=$latest_version
 export FRPS_INIT="https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/frps.init"
 export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
